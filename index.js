b411e8423e4f42f8856611f3bc829189ba74621b
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2022-02-02 07:17:43 -0500

    Added class-log file and finished class 29

--- index.js
+++ index.js
@@ -25,10 +25,14 @@ function clickRight() {
   {
     case -270:
       document.querySelector(".project1").setAttribute("tabindex", "-1");
+      document.querySelector(".project1-container").setAttribute("aria-hidden", true);
+      document.querySelector(".project4-container").removeAttribute("aria-hidden");
       document.querySelector(".project4").removeAttribute("tabindex");
       break;
     case -540:
       document.querySelector(".project2").setAttribute("tabindex", "-1");
+      document.querySelector(".project2-container").setAttribute("aria-hidden", true);
+      document.querySelector(".project5-container").removeAttribute("aria-hidden");
       document.querySelector(".project5").removeAttribute("tabindex");
       break;
     default:
@@ -51,10 +55,14 @@ function clickLeft() {
   {
     case -270:
       document.querySelector(".project5").setAttribute("tabindex", "-1");
+      document.querySelector(".project5-container").setAttribute("aria-hidden", true);
+      document.querySelector(".project2-container").removeAttribute("aria-hidden");
       document.querySelector(".project2").removeAttribute("tabindex");
       break;
     case 0:
       document.querySelector(".project4").setAttribute("tabindex", "-1");
+      document.querySelector(".project4-container").setAttribute("aria-hidden", true);
+      document.querySelector(".project1-container").removeAttribute("aria-hidden");
       document.querySelector(".project1").removeAttribute("tabindex");
       break;
     default:
